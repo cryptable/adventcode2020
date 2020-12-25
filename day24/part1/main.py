eebd47820ec4c8590001b3c86d04426d61007ce1
David Tillemas (DDT) <david.tillemans@cryptable.org>
2020-12-25 18:46:08 +0100

    Second Try

--- day24/part1/main.py
+++ day24/part1/main.py
@@ -11,21 +11,6 @@ class Tile:
     def __init__(self, id):
         self.id = id
         self.color = 'white'
-        self.tiles = {}
-        self.tiles_adj = {}
-        self.tiles['e'] = None
-        self.tiles['se'] = None
-        self.tiles['sw'] = None
-        self.tiles['w'] = None
-        self.tiles['nw'] = None
-        self.tiles['ne'] = None
-        # tuple adjecent direction, direction new tile to add link
-        self.tiles_adj['e'] = [('se', 'sw'), ('ne', 'nw')]
-        self.tiles_adj['se'] = [('e', 'ne'), ('sw', 'w')]
-        self.tiles_adj['sw'] = [('se', 'e'), ('w', 'nw')]
-        self.tiles_adj['w'] = [('sw', 'se'), ('nw', 'ne')]
-        self.tiles_adj['nw'] = [('w', 'sw'), ('ne', 'e')]
-        self.tiles_adj['ne'] = [('nw', 'w'), ('e', 'se')]
 
     def flip(self):
         if self.color == 'white':
@@ -33,85 +18,165 @@ class Tile:
         else:
             self.color = 'white'
 
-    def add_tile(self, direction, new_tile):
-        if not self.tiles[direction]:
-            self.tiles[direction] = new_tile
-            new_tile.tiles[opposite(direction)] = self
-            for adj in self.tiles_adj[direction]:
-                if self.tiles[adj[0]]:
-                    new_tile.add_tile(adj[1], self.tiles[adj[0]])
+    def print(self):
+        print("Tile id {} is {}".format(self.id, self.color))
 
-        return new_tile
 
-    def get_tile(self, direction):
-        return self.tiles[direction]
+class TileFloor:
+
+    def __init__(self):
+        self.origin = (0, 0)
+        self.floor = [[Tile(0)]]
+        self.curr_pos = (0, 0)
+
+    def add_row(self):
+        self.floor.append([None] * len(self.floor[0]))
+
+    def insert_row(self):
+        self.floor.insert(0, [None] * len(self.floor[0]))
+        origin_row = self.origin[0]
+        if origin_row % 2 == 0:
+            self.origin = (self.origin[0]+1, self.origin[1])
+            self.curr_pos = (self.curr_pos[0]+1, self.curr_pos[1])
+        else:
+            self.origin = (self.origin[0]+1, self.origin[1]+1)
+            self.curr_pos = (self.curr_pos[0]+1, self.curr_pos[1]+1)
+
+    def add_col(self):
+        for tile_row in self.floor:
+            tile_row.append(None)
+
+    def insert_col(self, update_cur_pos=False):
+        for tile_row in self.floor:
+            tile_row.insert(0, None)
+        origin_row = self.origin[0]
+        if origin_row % 2 == 1:
+            self.origin = (self.origin[0], self.origin[1]+1)
+#        curr_pos_row = self.curr_pos[0]
+#        if update_cur_pos:
+#            self.curr_pos = (self.curr_pos[0], self.curr_pos[1] + 1)
+#        else:
+        if origin_row % 2 == 1:
+            self.curr_pos = (self.curr_pos[0], self.curr_pos[1]+1)
 
     def print(self):
-        print("Tile id {}".format(self.id))
-        for (key, val) in self.tiles.items():
-            if val:
-                print("Connection {} {}".format(key, val.id))
+        alternate = True
+        for tile_row in self.floor:
+            if alternate:
+                print(' ', end='')
+            for tile in tile_row:
+                if tile:
+                    if tile.color == 'black':
+                        print('B', end=' ')
+                    else:
+                        print('W', end=' ')
+                else:
+                    print('N', end=' ')
+            alternate = not alternate
+            print()
 
+    def print_id(self):
+        alternate = True
+        for tile_row in self.floor:
+            if alternate:
+                print(' ', end='')
+            for tile in tile_row:
+                if tile:
+                    print('{}'.format(tile.id), end=' ')
+                else:
+                    print('N', end=' ')
+            alternate = not alternate
+            print()
+        print('Origin {} {}'.format(self.origin[0], self.origin[1]))
+        print('Cur Pos {} {}'.format(self.curr_pos[0], self.curr_pos[1]))
 
-def processTile(curr_tile, direction, id, tiles):
-    tile = None
+    def count_black(self):
+        count_black = 0
+        for tile_row in self.floor:
+            for tile in tile_row:
+                if tile and tile.color == 'black':
+                    count_black += 1
+        return count_black
 
-    if curr_tile.get_tile(direction):
-        tile = curr_tile.get_tile(direction)
-    else:
-        id += 1
-        tile = curr_tile.add_tile(direction, Tile(id))
-        tiles.append(tile)
+    def create_floor(self, line):
+        pos = 0
+        id = 1
+        while pos < len(line):
+            if line[pos:].startswith('se'):
+                if self.curr_pos[0] == len(self.floor) - 1:
+                    self.add_row()
+                if self.curr_pos[1] == len(self.floor[0]) - 1:
+                    self.add_col()
+                self.curr_pos = (self.curr_pos[0] + 1, (self.curr_pos[1] + 1) if self.curr_pos[0] % 2 == 0 else self.curr_pos[1])
+                if not self.floor[self.curr_pos[0]][self.curr_pos[1]]:
+                    self.floor[self.curr_pos[0]][self.curr_pos[1]] = Tile(id)
+                pos += 2
+            elif line[pos:].startswith('sw'):
+                print("sw1 {} {}".format(self.curr_pos[0], self.curr_pos[1]))
+                if self.curr_pos[0] == len(self.floor) - 1:
+                    self.add_row()
+                print("sw2 {} {}".format(self.curr_pos[0], self.curr_pos[1]))
+                if self.curr_pos[1] == 0:
+                    self.insert_col()
+                print("sw3 {} {}".format(self.curr_pos[0], self.curr_pos[1]))
+                self.curr_pos = (self.curr_pos[0] + 1,
+                                (self.curr_pos[1] - 1) if self.origin[0] % 2 == 1 else self.curr_pos[1])
+                print("sw4 {} {}".format(self.curr_pos[0], self.curr_pos[1]))
+                if not self.floor[self.curr_pos[0]][self.curr_pos[1]]:
+                    self.floor[self.curr_pos[0]][self.curr_pos[1]] = Tile(id)
+                self.print_id()
+                pos += 2
+            elif line[pos:].startswith('ne'):
+                if self.curr_pos[0] == 0:
+                    self.insert_row()
+                if self.curr_pos[1] == len(self.floor[0]) - 1:
+                    self.add_col()
+                self.curr_pos = (self.curr_pos[0] - 1, (self.curr_pos[1] + 1) if self.curr_pos[0] % 2 == 0 else self.curr_pos[1])
+                if not self.floor[self.curr_pos[0]][self.curr_pos[1]]:
+                    self.floor[self.curr_pos[0]][self.curr_pos[1]] = Tile(id)
+                pos += 2
+            elif line[pos:].startswith('nw'):
+                if self.curr_pos[0] == 0 and self.curr_pos[1] == 0:
+                    self.insert_row()
+                    if self.origin[0] % 2 == 1:
+                        self.insert_col()
+                elif self.curr_pos[0] == 0:
+                    self.insert_row()
+                elif self.curr_pos[1] == 0:
+                    self.insert_col()
+                self.curr_pos = (self.curr_pos[0] - 1,
+                                (self.curr_pos[1] - 1) if self.curr_pos[0] % 2 == 1 else self.curr_pos[1])
+                if not self.floor[self.curr_pos[0]][self.curr_pos[1]]:
+                    self.floor[self.curr_pos[0]][self.curr_pos[1]] = Tile(id)
+                pos += 2
+            elif line[pos:].startswith('e'):
+                if self.curr_pos[1] == len(self.floor[0]) - 1:
+                    self.add_col()
+                self.curr_pos = (self.curr_pos[0], self.curr_pos[1] + 1)
+                if not self.floor[self.curr_pos[0]][self.curr_pos[1]]:
+                    self.floor[self.curr_pos[0]][self.curr_pos[1]] = Tile(id)
+                pos += 1
+            elif line[pos:].startswith('w'):
+                if self.curr_pos[1] == 0:
+                    self.insert_col(True)
+                self.curr_pos = (self.curr_pos[0], self.curr_pos[1] - 1)
+                if not self.floor[self.curr_pos[0]][self.curr_pos[1]]:
+                    self.floor[self.curr_pos[0]][self.curr_pos[1]] = Tile(id)
+                pos += 1
+            else:
+                raise Exception("Unknown command")
+            id += 1
 
-    return (id, tiles, tile)
-
-
-def processLine(line, tiles, id):
-    pos = 0
-    curr_tile = tiles[0]
-
-    while pos < len(line):
-        if line[pos:].startswith('se'):
-            (id, tiles, curr_tile) = processTile(curr_tile, 'se', id, tiles)
-            pos += 2
-        if line[pos:].startswith('sw'):
-            (id, tiles, curr_tile) = processTile(curr_tile, 'sw', id, tiles)
-            pos += 2
-        if line[pos:].startswith('ne'):
-            (id, tiles, curr_tile) = processTile(curr_tile, 'ne', id, tiles)
-            pos += 2
-        if line[pos:].startswith('nw'):
-            (id, tiles, curr_tile) = processTile(curr_tile, 'nw', id, tiles)
-            pos += 2
-        if line[pos:].startswith('e'):
-            (id, tiles, curr_tile) = processTile(curr_tile, 'e', id, tiles)
-            pos += 1
-        if line[pos:].startswith('w'):
-            (id, tiles, curr_tile) = processTile(curr_tile, 'w', id, tiles)
-            pos += 1
-        print("--- Added tile : {}".format(curr_tile.id))
-        curr_tile.print()
-
-    curr_tile.flip()
-
-
-    return (id, tiles)
+        self.floor[self.curr_pos[0]][self.curr_pos[1]].flip()
 
 
 if __name__ == '__main__':
     id = 0
-    tiles = [Tile(id)]
+    floor = TileFloor()
 
-    with open("input_test2.txt", "r") as inFile:
+    with open("input_test4.txt", "r") as inFile:
         for line in inFile:
-            (id, tiles) = processLine(line, tiles, id)
+            floor.create_floor(line.rstrip('\n'))
 
-    count_black_tiles = 0
-    for tile in tiles:
-        if tile.color == 'black':
-            count_black_tiles += 1
-
-
-    print("{} of {} tiles".format(count_black_tiles, len(tiles)))
-    for tile in tiles:
-        tile.print()
+    floor.print_id()
+    print("nbr blacks: {}".format(floor.count_black()))
